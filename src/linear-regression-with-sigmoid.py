31aeb4dd4c07d34d9b8ba3654ffb10ab8683790f
J911 <jaemin.lee.email@gmail.com>
2019-05-13 13:48:39 +0900

    add: cross entory

--- src/linear-regression-with-sigmoid.py
+++ src/linear-regression-with-sigmoid.py
@@ -1,5 +1,5 @@
 import random
-from sympy import Symbol, diff, lambdify, exp
+from sympy import Symbol, diff, lambdify, exp, log
 import numpy as np
 
 def sigmoid(x):
@@ -24,11 +24,22 @@ class Model():
         hypothesis = sigmoid(x[:,0] * self.weight1 + x[:,1] * self.weight2 + self.bias)
         output = lambdify((self.weight1, self.weight2, self.bias), hypothesis)
         return output(self._weight1, self._weight2, self._bias), hypothesis
-    
+
+    def MSE(self, pred, label):
+        cost = np.square(label - pred).sum() / label.size
+        return cost
+
+    def CrossEntropy(self, pred, label):
+        cost = 0
+        for p, l in zip(pred, label):
+            cost += l * log(p) + (1 - l) * log(1 - p)
+        cost = - 1 * cost / label.size
+        return cost
+
     def train(self, x, y):
         x, y = np.array(x, dtype='float'), np.array(y, dtype='float')
         _, hypothesis = self.forward(x)
-        cost = np.square(y - hypothesis).sum() / y.size
+        cost = self.MSE(hypothesis, y)
 
         weight1_diff = lambdify((self.weight1, self.weight2, self.bias), diff(cost, self.weight1))
         weight2_diff = lambdify((self.weight1, self.weight2, self.bias), diff(cost, self.weight2))
@@ -48,10 +59,10 @@ if __name__ == "__main__":
     y_data = [0, 0, 0, 1]
 
     # generate Model instance
-    model = Model(lr = 0.1)
+    model = Model(lr = 1)
 
     # train 1000 iter
-    for step in range(1, 1001):
+    for step in range(1, 101):
         loss = model.train(x_data, y_data)
         print(step, 'iter', 'loss:', loss)
 
